d8b3c15d4a68f677c92a642c6ae68ec26c892542
Lawando69 <lawand15.kong@gmail.com>
2026-02-10 14:07:04 +0000

    feat: added the taking picture file that was completed during teh lab session with tutor

---
+++ CS_Year1_Projects/src/main/OpenCVStill.java
@@ -0,0 +1,15 @@
+import swiftbot.*;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+ 
+public class OpenCVStill{
+       public static void main(String args[]) throws Exception
+       {
+              SwiftBotAPI sb = swiftbot.SwiftBotAPI.INSTANCE;
+              BufferedImage img = sb.takeStill(ImageSize.SQUARE_1080x1080);
+              ImageIO.write(img, "jpg", new File("/data/home/pi/TestImage.jpg"));
+ 
+              System.exit(1);
+       }
+}
