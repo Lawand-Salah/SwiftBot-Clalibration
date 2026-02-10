161427206121d7fe3baf9715c9b84427a8aad8d7
Lawando69 <lawand15.kong@gmail.com>
2026-02-10 13:45:06 +0000

    organize: the folders have been organised & some lab worksheets have been added for tuesday seminar and lab

---
+++ CS_Year1_Projects/src/main/SnakesLadders.java
@@ -0,0 +1,14 @@
+import swiftbot.Button;
+
+public class SnakesLadders {
+
+	public static void main(String[] args) {
+		testMethod();
+
+	}
+	
+	static void testMethod() {
+		System.out.println("This is a runnning test!");
+	}
+
+}
